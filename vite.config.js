bfc59a7a412436ff44c273e092632f3193f64b71
devkoow <qyaka178@naver.com>
2024-07-27 16:40:59 +0900

    [Docs] vite.config.js 'base' 추가

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import viteCompression from 'vite-plugin-compression';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+  base: '/mr-cryp/',
   plugins: [
     react(),
     viteCompression({
